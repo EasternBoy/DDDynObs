6fd7cb3582afc1bd9ee1261da2003565d6f23455
BINH NGUYEN <121826237+EasternBoy@users.noreply.github.com>
2024-07-18 16:45:36 -0500

    Update computing.jl

--- computing.jl
+++ computing.jl
@@ -6,15 +6,25 @@ function Detection!(robo::robot, obs::obstacle, obsGP::Vector{GPBase}, mNN::Chai
     if norm(robo_loca - obs_loca) < R
         for i in 1:length(obsGP)
             append!(obsGP[i], reshape(obs.time,1,length(obs.time)), obs.mulpos[i,:])
-            if length(obsGP[i].x) >= 6
-                method = NelderMead(;   parameters = Optim.AdaptiveParameters(),
-                                        initial_simplex = Optim.AffineSimplexer())
-                obsGP[i].logNoise.value = -2.
-                obsGP[i].kernel.σ2      =  1.
 
-                obsGP[i].kernel.iℓ2     = [10.]
-                GaussianProcesses.optimize!(obsGP[i], domean = true, kern = true, noise = true, 
-                                            noisebounds = [-3., -1.], kernbounds = [[-5.,-5.],[5.,5.]]; method)
+            bestmodel = obsGP[i]
+            bestmll = -Inf
+            if length(obsGP[i].x) >= 6
+                
+
+                for il2 in [0.1, 1., 10.]
+                    obsGP[i].kernel.iℓ2[1]  =  il2
+                    obsGP[i].logNoise.value = -2.
+                    obsGP[i].kernel.σ2      =  1.
+
+                    GaussianProcesses.optimize!(obsGP[i], domean = true, kern = true, noise = true, 
+                                                noisebounds = [-3., -1.], kernbounds = [[-5.,-5.],[5.,5.]]; method = NelderMead())
+                    if bestmll < obsGP[i].mll
+                        bestmll   = obsGP[i].mll
+                        bestmodel = deepcopy(obsGP[i])
+                    end
+                end
+                obsGP[i] = bestmodel
             end
         end
         
@@ -77,38 +87,25 @@ function predictLSTM(mNN::Chain, dataIn::Vector{Float64}; recal = 20) #time seri
 end
 
 
-function SEkern!(mGP::GPBase, X1::Vector{Float64}, X2::Vector{Float64})
-    row   = length(X1)
-    col   = length(X2)
-    K     = zeros(row, col)
-    σκ2   = mGP.kernel.σ2
-    iℓ2   = mGP.kernel.iℓ2
-
-    for i in 1:row
-        for j in 1:col
-            K[i,j] = σκ2*exp(-0.5*iℓ2[1]*(X1[i] - X2[j])^2)
-        end
-    end
-    return K
-end
-
 function predictGP(mNN::Chain, mGP::GPBase, dataIn::Vector{Float64}, id::Int64)
     H = length(dataIn)
     postMean = ones(H)
     postVar  = zeros(H)
 
-    t     = [x for x in mGP.x][1,:]
-    ndata = length(t)
-    Ktt   = SEkern!(mGP, t, t)
+    t     = obsGP[1].x.data
+    
+    Ktt   = cov(mGP.kernel, mGP.x, mGP.x)
     σω2   = exp(2*mGP.logNoise.value)
+
+    ndata = length(t)
     Cyy   = inv(Ktt + σω2*Matrix(I, ndata, ndata))
 
     Y = [y for y in mGP.y]  
 
     for i in 1:H
         h   = dataIn[i]
-        Kth = SEkern!(mGP, t, [h])
-        Khh = SEkern!(mGP, [h], [h])
+        Kth = cov(mGP.kernel, mGP.x, [h;;])
+        Khh = cov(mGP.kernel, [h;;], [h;;])
 
         postMean[i] = predictLSTM(mNN, [h])[id] + (Kth'*Cyy*(Y - predictLSTM(mNN, t)[id,:]))[1]
         postVar[i]  = (Khh - Kth'*Cyy*Kth)[1]
