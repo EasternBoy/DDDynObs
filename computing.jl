e8e001c05f0002ec270e7e23245e3dba79b61d6f
BINH NGUYEN <121826237+EasternBoy@users.noreply.github.com>
2024-07-23 17:02:23 -0500

    Update computing.jl

--- computing.jl
+++ computing.jl
@@ -1,4 +1,4 @@
-function Detection!(robo::robot, obs::obstacle, obsGP::Vector{GPBase}, mNN::Chain)
+function Detection!(robo::robot, obs::obstacle, obsGP::Vector{GPBase}, mNN::Chain; minData = 10)
     robo_loca = robo.pose[1:2]
     obs_loca  = obs.posn
     R         = robo.R
@@ -6,32 +6,31 @@ function Detection!(robo::robot, obs::obstacle, obsGP::Vector{GPBase}, mNN::Chai
     if norm(robo_loca - obs_loca) < R
         for i in 1:length(obsGP)
             append!(obsGP[i], reshape(obs.time,1,length(obs.time)), obs.mulpos[i,:])
-
+        end
+        
+        for i in 1:length(obsGP)
             bestmodel = obsGP[i]
-            bestmll = -Inf
-            if length(obsGP[i].x) >= 6
-                
-
+            bestmll   = -Inf
+            if length(obsGP[i].x) >= minData
                 for il2 in [0.1, 1., 10.]
                     obsGP[i].kernel.iℓ2[1]  =  il2
                     obsGP[i].logNoise.value = -2.
                     obsGP[i].kernel.σ2      =  1.
-
-                    GaussianProcesses.optimize!(obsGP[i], domean = true, kern = true, noise = true, 
-                                                noisebounds = [-3., -1.], kernbounds = [[-5.,-5.],[5.,5.]]; method = NelderMead())
+                    GaussianProcesses.optimize!(obsGP[i], domean = false, kern = true, noise = true, noisebounds = [-3., -1.], 
+                                                kernbounds = [[-5.,-5.],[5.,5.]], method = Optim.NelderMead())
                     if bestmll < obsGP[i].mll
                         bestmll   = obsGP[i].mll
                         bestmodel = deepcopy(obsGP[i])
                     end
                 end
                 obsGP[i] = bestmodel
+                t = [x for x in obsGP[1].x][1,:]
+                Y = transpose([[y for y in obsGP[1].y] [y for y in obsGP[2].y]])
+                trainNN(mNN, t, Y)
+            else
+                GaussianProcesses.optimize!(obsGP[i], domean = true, kern = true, noise = true, 
+                                            noisebounds = [-3., -1.], kernbounds = [[-5.,-5.],[5.,5.]])
             end
-        end
-        
-        if length(obsGP[1].x) >= 10
-            t = [x for x in obsGP[1].x][1,:]
-            Y = transpose([[y for y in obsGP[1].y] [y for y in obsGP[2].y]])
-            trainNN(mNN, t, Y)
         end
     end
 end
@@ -39,8 +38,9 @@ end
 
 function trainNN(mNN::Chain, dataIn::AbstractVector, dataOut::AbstractArray; epochs = 400) #time series
 
-    X = [[Float32.(x)] for x in dataIn]
-    Y = Float32.(dataOut)
+    X   = [[Float32.(x)] for x in dataIn]
+    Y   = Float32.(dataOut)
+    dim = minimum(size(dataOut))
 
     bestloss        = Inf
     bestmodel       = mNN
@@ -54,11 +54,11 @@ function trainNN(mNN::Chain, dataIn::AbstractVector, dataOut::AbstractArray; epo
         Flux.reset!(mNN)
         ∇ = gradient(θ) do
             mNN(X[1]) # Warm-up the model
-            sum(sum(Flux.Losses.mse.([mNN(x)[i] for x in X[1:end]], Y[i,1:end])) for i in 1:2)
+            sum(sum(Flux.Losses.mse.([mNN(x)[i] for x in X], Y[i,:])) for i in 1:dim)
         end
         Flux.update!(opt, θ, ∇)
 
-        loss = sum(sum(Flux.Losses.mse.([mNN(x)[i] for x in X[1:end]], Y[i,1:end])) for i in 1:2)
+        loss = sum(sum(Flux.Losses.mse.([mNN(x)[i] for x in X], Y[i,:])) for i in 1:dim)
 
         if loss < 0.95bestloss
             bestloss  = loss
@@ -87,7 +87,7 @@ function predictLSTM(mNN::Chain, dataIn::Vector{Float64}; recal = 20) #time seri
 end
 
 
-function predictGP(mNN::Chain, mGP::GPBase, dataIn::Vector{Float64}, id::Int64)
+function predictGP(mNN::Chain, mGP::GPBase, dataIn::Vector{Float64}, id::Int64; minData = 10)
     H = length(dataIn)
     postMean = ones(H)
     postVar  = zeros(H)
@@ -103,12 +103,17 @@ function predictGP(mNN::Chain, mGP::GPBase, dataIn::Vector{Float64}, id::Int64)
     Y = [y for y in mGP.y]  
 
     for i in 1:H
-        h   = dataIn[i]
-        Kth = cov(mGP.kernel, mGP.x, [h;;])
-        Khh = cov(mGP.kernel, [h;;], [h;;])
+        if length(mGP.x) >= minData
+            h   = dataIn[i]
+            Kth = cov(mGP.kernel, mGP.x, [h;;])
+            Khh = cov(mGP.kernel, [h;;], [h;;])
 
-        postMean[i] = predictLSTM(mNN, [h])[id] + (Kth'*Cyy*(Y - predictLSTM(mNN, t)[id,:]))[1]
-        postVar[i]  = (Khh - Kth'*Cyy*Kth)[1]
+            postMean[i] = predictLSTM(mNN, [h])[id] + (Kth'*Cyy*(Y - predictLSTM(mNN, t)[id,:]))[1]
+            postVar[i]  = (Khh - Kth'*Cyy*Kth)[1]
+        else
+            postMean[i], var = predict_f(mGP, [dataIn[i];;] ; full_cov = true)
+            postVar[i] = var[1]
+        end
     end
 
     return postMean, postVar
