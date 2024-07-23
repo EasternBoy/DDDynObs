b6181c38ef84168d66a123bf9721a00bfa9f77b2
BINH NGUYEN <121826237+EasternBoy@users.noreply.github.com>
2024-07-23 16:59:06 -0500

    Update Sim.jl

--- Sim.jl
+++ Sim.jl
@@ -1,13 +1,13 @@
 push!(LOAD_PATH, ".")
-dim = 2; T = 0.1; H = 5; L = 20
+dim = 2; T = 0.1; H = 5; L = 20; ns = 3
 
 
 import Pkg
 using Pkg
 Pkg.activate(@__DIR__)
 
-using Optim, Random, Distributions, CSV, DataFrames, MAT
-using Plots, Dates, Statistics, Colors, ColorSchemes, StatsPlots
+using Optim, Random, CSV, DataFrames, MAT,  Distributions
+using Plots, Dates, Statistics, Colors, ColorSchemes
 using Ipopt, JuMP, GaussianProcesses, LinearAlgebra, OrdinaryDiffEq, Flux
 
 include("objects.jl")
@@ -28,8 +28,8 @@ color =  cgrad(:turbo, 2, categorical = true, scale = :lin)
 
 
 R = 40.
-r = 3.
-str_ang = 0.6
+ℓ = 1.9
+str_ang =  0.6
 v_min   = -1.
 v_max   =  2.
 a_min   = -1.
@@ -37,18 +37,18 @@ a_max   =  1.
 pBounds = polyBound(str_ang, v_min, v_max, a_min, a_max)
 
 
-init  = [0., 0., 0., 0.]
-A     = [1. -1. 0. 0.; 0. 0. 1. -1.]
+init  = [0., 3., 0., 0.]
+A     = [1. 0; -1. 0.; 0. 1.; 0. -1.]
 b     = [2.5, 2.5, 1., 1.]
 
 
-obs   = obstacle(2., [10., 0.], 3)
-robo  = robot(T, H, R, r, pBounds, init, A, b)
+obs   = obstacle(2., [20., 0.], ns)
+robo  = robot(T, H, R, ℓ, pBounds, init, A, b)
 
 obsGP    = Vector{GPBase}(undef, 2) #MeanPoly(ones(1,10))
-obsGP[1] = ElasticGPE(1, mean = MeanZero(), kernel = SEArd([1.], 1.), logNoise = -2.)
-obsGP[2] = ElasticGPE(1, mean = MeanZero(), kernel = SEArd([1.], 1.), logNoise = -2.)
-mNN      = Chain(LSTM(1 => 32), Dense(32 => 2, identity))
+obsGP[1] = ElasticGPE(1, mean = MeanConst(1.), kernel = SEArd([1.], 1.), logNoise = -2.)
+obsGP[2] = ElasticGPE(1, mean = MeanConst(1.), kernel = SEArd([1.], 1.), logNoise = -2.)
+mNN      = Chain(LSTM(1 => 15), LSTM(15 => 2))
 
 
 # Run simulation
@@ -62,44 +62,51 @@ for k in 1:L
     println("Time instance $k")
     global Pmean
 
-    run_obs!(obs, k, T, 1)
-
-    # Train
+    TypeOfObs = 1
+    run_obs!(obs, k-1, T, TypeOfObs)
 
     @time Detection!(robo, obs, obsGP, mNN)
+    Ref  = [[5(k+i)*T, 3.] for i in 1:H]
 
     if k >= 10
         for i in 1:dim
             Pmean[i,:], Pvar[i,:] = predictGP(mNN, obsGP[i], k*T .+ T*[x for x in 1:H], i)
         end
 
-        fig = plot(obs.traj[1,:], obs.traj[2,:])
-        png(fig, "Step $k")
+        Prob = [0.95*0.8^(i-1)  for i in 1:H]
+        φ    = [quantile(Normal(0.,1.), (1 + Prob[i])/2)  for i in 1:H]
+        
+        fig = plot(obs.traj[1,:], obs.traj[2,:], label = "Past trajactory")
+        plot!([obs.posn[1]; Pmean[1,:]], [obs.posn[2]; Pmean[2,:]], label = "Predicted mean trajectory")
+        xlims!((2.,20.)); ylims!((-5.,10.))
+
+        for i in 1:H
+            δ₁  = φ[i]*sqrt(Pvar[1,i]) 
+            δ₂  = φ[i]*sqrt(Pvar[2,i])
+            rec = Shape([Pmean[1,i] + δ₁, Pmean[1,i] + δ₁, Pmean[1,i] - δ₁, Pmean[1,i] - δ₁],
+                        [Pmean[2,i] + δ₂, Pmean[2,i] - δ₂, Pmean[2,i] - δ₂, Pmean[2,i] + δ₂])
+            CI = Int64(round(Prob[i]*100))
+            plot!(rec, label="$CI% Con. In. at $i step ahead")
+        end
+
+        θ = robo.pose[3]
+        p = robo.pose[1:2]
+        R = [cos(θ) -sin(θ); sin(θ) cos(θ)]
+        RU = R*b[1]
+        RD = R*b[2:3]
+        LU = 
+        LD = 
+        rec = Shape([p[1] + δ₁, p[1] + δ₁, p[1] - δ₁, p[1] - δ₁],
+                    [p[2] + δ₂, p[2] - δ₂, p[2] - δ₂, p[2] + δ₂])
+
+        png(fig, "Predict after $k-th step (collect $ns data each step)")
+
+        @time x, y, θ, v, tα, a = nMPC_avoid(robo, obs, Ref, Pmean, Pvar, φ)
+
+        run!(robo, [tα[1], a[1]])
+    else
+        @time x, y, θ, v, tα, a = nMPC_free(robo, Ref)
+        run!(robo, [tα[1], a[1]])
     end
+
 end
-
-
-
-# gr(size=(1000,600))
-# FigRMSE = errorline(1:L, RMSE[:,1:L], linestyles = :solid, linewidth=2, secondarylinewidth=2, xlims = (0,L+0.5), errorstyle=:stick, 
-# secondarycolor=:blue,  legendfontsize = 24, tickfontsize = 30, framestyle = :box, label = "")
-# # errorline!(1:L, RMSE, linestyles = :solid, linewidth=2, xlims = (0,L+0.5), errorstyle=:ribbon, label="")
-# scatter!(1:L, [mean(RMSE[:,i]) for i in 1:L], label="Mean Errors")
-# png(FigRMSE, "Figs-10robots-200/RMSE")
-
-# pResE    = zeros(N,MAX_ITER)
-# [pResE[i,:] = sum(ResE[:,i,k] for k in 1:L)/L for i in 1:N]
-# id       = minimum([nonzero(pResE[i,:],0.) for i in 1:N]) - 7
-# FigpResE = errorline(1:id, pResE[:,1:id], secondarylinewidth=2, secondarycolor=:blue, errorstyle=:stick, framestyle = :box, yticks = 10 .^(-3.:1.:2.), label = "",
-#             legendfontsize = 24, tickfontsize = 30, xlims = (0, id-0.5), ylims = (1e-3, 2e2),  yscale=:log10, linestyles = :solid, linewidth=2)
-# scatter!(1:id, [mean(pResE[:,k]) for k in 1:id], label="Mean Errors")
-# png(FigpResE, "Figs-10robots-200/ResE")
-
-                
-# png(plot(1:L, Eig2[1:L], linestyles = :dot, linewidth=3, xlims = (0,L+0.5), ylims = (0, 1.1*maximum(Eig2)), legendfontsize = 24,
-#                 tickfontsize = 30, markershape = :circle, markersize = 5, label="", framestyle = :box), "Figs-10robots-200/Eig2")
-
-# png(plot(1:L, minD[1:L], linestyles = :dot, linewidth=3, xlims = (0,L+0.5), ylims = (0, 1.1*maximum(minD)), legendfontsize = 24,
-#                 tickfontsize = 30, markershape = :circle, markersize = 5, label="", framestyle = :box), "Figs-10robots-200/minD")
-
-# matwrite("Data10robo200.mat", Dict("RMSE" => RMSE, "ResE" => ResE, "Eig2" => Eig2, "PosX" => PosX, "PosY" => PosY))
