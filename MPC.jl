1043fa6967f24a58bf9aba6256d137339e0b36a5
BINH NGUYEN <121826237+EasternBoy@users.noreply.github.com>
2024-07-23 17:01:14 -0500

    Add files via upload

---
+++ MPC.jl
@@ -0,0 +1,121 @@
+function nMPC_avoid(robo::robot, obs::obstacle, ref::Vector{Vector{Float64}}, Pmean::Matrix{Float64}, Pvar::Matrix{Float64}, φ::Vector{Float64})
+    
+    opti  = JuMP.Model(Ipopt.Optimizer)
+    set_silent(opti)
+
+    H     = robo.H
+    τ     = robo.T
+    A     = robo.A
+    b     = robo.b
+    E     = A
+    ℓ     = robo.ℓ
+    cᵣ    = obs.r
+
+
+    d = [φ[i]*[sqrt(Pvar[1,i]), sqrt(Pvar[1,i]), sqrt(Pvar[2,i]), sqrt(Pvar[2,i])] for i in 1:H]
+
+    bds   = robo.pBnd
+    state = robo.pose
+
+    dimR = length(robo.b)
+    dimO = length(d[1])
+
+    @variable(opti, x[1:H])
+    @variable(opti, y[1:H])
+    @variable(opti, θ[1:H])
+    @variable(opti, v[1:H])
+    @variable(opti, tα[1:H])
+    @variable(opti, a[1:H])
+    @variable(opti, λ[1:dimR,1:H])
+    @variable(opti, μ[1:dimO,1:H])
+
+    J = sum(dot([x[i], y[i]] - ref[i], [x[i], y[i]] - ref[i]) for i in 1:H)
+    @objective(opti, Min, J)
+
+    @constraints(opti, begin    bds.v_max  .>= v  .>= -bds.v_min
+                                bds.a_max  .>= a  .>= -bds.a_min
+                                tan(bds.str_ang) .>= tα .>= -tan(bds.str_ang) end)
+
+    @constraint(opti, λ .>= 0)
+    @constraint(opti, μ .>= 0)
+
+    for i in 1:H
+        if i == 1
+            @constraint(opti, x[i] == state[1] + τ*cos(state[3]))
+            @constraint(opti, y[i] == state[2] + τ*sin(state[3]))
+            @constraint(opti, θ[i] == state[3] + τ*tα[i]/ℓ)
+            @constraint(opti, v[i] == state[4] + τ*a[i])
+        else
+            @constraint(opti, x[i] == x[i-1] + τ*cos(θ[i-1]))
+            @constraint(opti, y[i] == y[i-1] + τ*sin(θ[i-1]))
+            @constraint(opti, θ[i] == θ[i-1] + τ*tα[i]/ℓ)
+            @constraint(opti, v[i] == v[i-1] + τ*a[i])
+        end
+
+        @constraint(opti, dot(E'*μ[:,i], E'*μ[:,i]) <= 1)
+        @constraint(opti, transpose(E*([x[i], y[i]] - Pmean[:,i]) - d[i])*μ[:,i]- b'*λ[:,i] >= cᵣ)
+        @constraint(opti, λ[:,i]'*A + μ[:,i]'*E*[cos(θ[i]) -sin(θ[i]); sin(θ[i]) cos(θ[i])] .== 0)
+    end
+
+    JuMP.optimize!(opti)
+
+    x  = JuMP.value.(x)
+    y  = JuMP.value.(y)
+    θ  = JuMP.value.(θ)
+    v  = JuMP.value.(v)
+    tα = JuMP.value.(tα) 
+    a  = JuMP.value.(a)
+
+    return x, y, θ, v, tα, a
+end
+
+function nMPC_free(robo::robot, ref::Vector{Vector{Float64}})
+
+    opti  = JuMP.Model(Ipopt.Optimizer)
+    set_silent(opti)
+
+    H     = robo.H
+    τ     = robo.T
+    ℓ     = robo.ℓ
+    bds   = robo.pBnd
+    state = robo.pose
+
+    @variable(opti, x[1:H])
+    @variable(opti, y[1:H])
+    @variable(opti, θ[1:H])
+    @variable(opti, v[1:H])
+    @variable(opti, tα[1:H])
+    @variable(opti, a[1:H])
+
+    J = sum(dot([x[i], y[i]] - ref[i], [x[i], y[i]] - ref[i]) for i in 1:H)
+    @objective(opti, Min, J)
+
+    @constraints(opti, begin    bds.v_max  .>= v  .>= -bds.v_min
+                                bds.a_max  .>= a  .>= -bds.a_min
+                                tan(bds.str_ang) .>= tα .>= -tan(bds.str_ang) end)
+    for i in 1:H
+        if i == 1
+            @constraint(opti, x[i] == state[1] + τ*cos(state[3]))
+            @constraint(opti, y[i] == state[2] + τ*sin(state[3]))
+            @constraint(opti, θ[i] == state[3] + τ*tα[i]/ℓ)
+            @constraint(opti, v[i] == state[4] + τ*a[i])
+        else
+            @constraint(opti, x[i] == x[i-1] + τ*cos(θ[i-1]))
+            @constraint(opti, y[i] == y[i-1] + τ*sin(θ[i-1]))
+            @constraint(opti, θ[i] == θ[i-1] + τ*tα[i]/ℓ)
+            @constraint(opti, v[i] == v[i-1] + τ*a[i])
+        end
+    end
+
+    JuMP.optimize!(opti)
+
+    x  = JuMP.value.(x)
+    y  = JuMP.value.(y)
+    θ  = JuMP.value.(θ)
+    v  = JuMP.value.(v)
+    tα = JuMP.value.(tα) 
+    a  = JuMP.value.(a)
+
+    return x, y, θ, v, tα, a
+    
+end
