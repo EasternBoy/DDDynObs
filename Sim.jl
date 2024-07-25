d26f149d1872c130714cd609a4da76050356f56a
BINH NGUYEN <121826237+EasternBoy@users.noreply.github.com>
2024-07-24 19:22:42 -0500

    Update Sim.jl

--- Sim.jl
+++ Sim.jl
@@ -1,14 +1,21 @@
 push!(LOAD_PATH, ".")
-dim = 2; T = 0.1; H = 5; L = 20; ns = 3
+dim = 2 
+T   = 0.1
+H   = 5
+H₊  = 8
+L   = 25 
+ns  = 3
 
 
 import Pkg
 using Pkg
 Pkg.activate(@__DIR__)
 
-using Optim, Random, CSV, DataFrames, MAT,  Distributions
+using Optim, Random, CSV, DataFrames, MAT,  Distributions, NLopt
 using Plots, Dates, Statistics, Colors, ColorSchemes
 using Ipopt, JuMP, GaussianProcesses, LinearAlgebra, OrdinaryDiffEq, Flux
+
+
 
 include("objects.jl")
 include("computing.jl")
@@ -30,20 +37,20 @@ color =  cgrad(:turbo, 2, categorical = true, scale = :lin)
 R = 40.
 ℓ = 1.9
 str_ang =  0.6
-v_min   = -1.
-v_max   =  2.
-a_min   = -1.
-a_max   =  1.
+v_min   = -20.
+v_max   =  20.
+a_min   = -40.
+a_max   =  10.
 pBounds = polyBound(str_ang, v_min, v_max, a_min, a_max)
 
 
 init  = [0., 3., 0., 0.]
 A     = [1. 0; -1. 0.; 0. 1.; 0. -1.]
-b     = [2.5, 2.5, 1., 1.]
+b     = [2.3, 2.3, 1., 1.]
 
 
 obs   = obstacle(2., [20., 0.], ns)
-robo  = robot(T, H, R, ℓ, pBounds, init, A, b)
+robo  = robot(T, H, H₊, R, ℓ, pBounds, init, A, b)
 
 obsGP    = Vector{GPBase}(undef, 2) #MeanPoly(ones(1,10))
 obsGP[1] = ElasticGPE(1, mean = MeanConst(1.), kernel = SEArd([1.], 1.), logNoise = -2.)
@@ -66,19 +73,20 @@ for k in 1:L
     run_obs!(obs, k-1, T, TypeOfObs)
 
     @time Detection!(robo, obs, obsGP, mNN)
-    Ref  = [[5(k+i)*T, 3.] for i in 1:H]
+    Ref  = [[5(k+i-1)*T, 3.] for i in 1:H₊]
 
     if k >= 10
         for i in 1:dim
             Pmean[i,:], Pvar[i,:] = predictGP(mNN, obsGP[i], k*T .+ T*[x for x in 1:H], i)
         end
 
-        Prob = [0.95*0.8^(i-1)  for i in 1:H]
+        # Prob = [0.95*0.8^(i-1)  for i in 1:H]
+        Prob = [0.95  for i in 1:H]
         φ    = [quantile(Normal(0.,1.), (1 + Prob[i])/2)  for i in 1:H]
         
-        fig = plot(obs.traj[1,:], obs.traj[2,:], label = "Past trajactory")
-        plot!([obs.posn[1]; Pmean[1,:]], [obs.posn[2]; Pmean[2,:]], label = "Predicted mean trajectory")
-        xlims!((2.,20.)); ylims!((-5.,10.))
+        fig = plot(obs.traj[1,:], obs.traj[2,:], label = "Past trajectory")
+        plot!([obs.posn[1]; Pmean[1,:]], [obs.posn[2]; Pmean[2,:]], label = "Predicted mean trajectory") 
+        xlims!((0.,20.)); ylims!((-2.,8.))
 
         for i in 1:H
             δ₁  = φ[i]*sqrt(Pvar[1,i]) 
@@ -86,18 +94,19 @@ for k in 1:L
             rec = Shape([Pmean[1,i] + δ₁, Pmean[1,i] + δ₁, Pmean[1,i] - δ₁, Pmean[1,i] - δ₁],
                         [Pmean[2,i] + δ₂, Pmean[2,i] - δ₂, Pmean[2,i] - δ₂, Pmean[2,i] + δ₂])
             CI = Int64(round(Prob[i]*100))
-            plot!(rec, label="$CI% Con. In. at $i step ahead")
+            plot!(rec, label="$CI% Con. In. at $i step ahead") #
         end
 
         θ = robo.pose[3]
         p = robo.pose[1:2]
-        R = [cos(θ) -sin(θ); sin(θ) cos(θ)]
-        RU = R*b[1]
-        RD = R*b[2:3]
-        LU = 
-        LD = 
-        rec = Shape([p[1] + δ₁, p[1] + δ₁, p[1] - δ₁, p[1] - δ₁],
-                    [p[2] + δ₂, p[2] - δ₂, p[2] - δ₂, p[2] + δ₂])
+        Rot = [cos(θ) -sin(θ); sin(θ) cos(θ)]
+        RU = Rot*[b[1], b[3]]
+        RD = Rot*[b[1],-b[3]]
+        LU = -RD
+        LD = -RU
+        rec = Shape([p[1]+RU[1], p[1]+RD[1], p[1]+LD[1], p[1]+LU[1]],
+                    [p[2]+RU[2], p[2]+RD[2], p[2]+LD[2], p[2]+LU[2]])
+        plot!(rec, label="Robo at step $k")
 
         png(fig, "Predict after $k-th step (collect $ns data each step)")
 
